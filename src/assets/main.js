22bfe9d22d59e9c22f8893334070936546859b6b
Steve Cottam <Steve Cottam>
2016-12-13 19:43:58 +0000

    updated main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,29 @@
 $(function() {
 
   // your code will go here
+  // https://www.codeschool.com/users/s_cot1983.json
+  $.ajax({
+		url: 'https://www.codeschool.com/users/s_cot1983.json',
+		dataType: 'jsonp',
+		success: function(response){
+			console.log('response',response.courses.completed);
+			addBadges(response.courses.completed)
+		}
+	  
+  });
+  function addBadges(courses) {
+	  var badges = $('#badges');
+	  courses.forEach(function(course){
+		var $course = $('<div />', {'class': 'course'}).appendTo(badges);
+		  $('<h3 />', {text: course.title}).appendTo($course);
+		  $('<img />', {src: course.badge}).appendTo($course);
+		  $('<a />', {
+			  href: course.url, 
+			  target: '_blank', 
+			  'class': 'btn btn-primary',
+			  text: 'See Course'}).appendTo($course);
+	  });
+  }
+  
 
 });
